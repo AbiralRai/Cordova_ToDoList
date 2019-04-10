370dc6d740f9d89faf4bc8238c3c121095728a22
Abiralrai <abiralrai182@gmail.com>
2019-04-10 18:31:04 +1000

    First commit for Cordova Class Assignment

---
+++ platforms/browser/www/js/index.js
@@ -0,0 +1,8 @@
+document.addEventListener('deviceready', init);
+
+document.addEventListener('DOMContentLoaded', () => { alert("Brower ready"); })
+
+function init() {
+    alert('Device ready');
+
+}
